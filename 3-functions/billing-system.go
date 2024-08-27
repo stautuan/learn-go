09e2e773fa01e6d4217d17e4209aa96eee233a8f
stautuan <stautuan89@gmail.com>
2024-08-27 13:44:59 -0400

    update

--- 3-functions/billing-system.go
+++ 3-functions/billing-system.go
@@ -1,3 +1,17 @@
+/*
+Textio is making some changes to how they bill users for bulk messages. 
+The system should now calculate the bill based on:
+    The number of messages sent
+    The cost per message
+    Thresholds for discounts
+
+Discount rates:
+    10% for more than 1000 messages.
+    20% for more than 5000 messages.
+    0% for anything less.
+*/
+
+
 package main
 
 
@@ -6,7 +20,6 @@ func calculateFinalBill(costPerMessage float64, numMessages int) float64 {
     discountRate := calculateDiscountRate(numMessages)
     final := cost * discountRate
     return final
-  
 }
 
 func calculateDiscountRate(messagesSent int) float64 {
@@ -17,7 +30,6 @@ func calculateDiscountRate(messagesSent int) float64 {
         return 0.9
     }
     return 1.0
-  
 }
 
 func calculateBaseBill(costPerMessage float64, messagesSent int) float64 {
