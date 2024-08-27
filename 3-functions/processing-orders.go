8fd3ecf2b11b730d4a429426906db77223da2912
stautuan <stautuan89@gmail.com>
2024-08-27 16:05:21 -0400

    processing orders

---
+++ 3-functions/processing-orders.go
@@ -0,0 +1,68 @@
+package main
+
+func placeOrder(productID string, quantity int, accountBalance float64) (bool, float64) {
+    inStock := amountInStock(productID)
+    if quantity > inStock {
+        return false, accountBalance
+    }
+
+    totalPrice := calcPrice(productID, quantity)
+    if accountBalance < totalPrice{
+        return false, accountBalance
+    }
+
+    newBalance := accountBalance - totalPrice
+    return true, newBalance
+}
+
+func calcPrice(productID string, quantity int) float64 {
+    return priceList(productID) * float64(quantity)
+}
+
+func priceList(productID string) float64 {
+    if productID == "1" {
+        return 1.50
+    } else if productID == "2" {
+        return 2.25
+    } else if productID == "3" {
+        return 3.00
+    } else if productID == "4" {
+        return 1.00
+    } else if productID == "5" {
+        return 2.50
+    } else if productID == "6" {
+        return 8.99
+    } else if productID == "7" {
+        return 22.50
+    } else if productID == "8" {
+        return 50.00
+    } else if productID == "9" {
+        return 999.99
+    } else {
+        return 0.00
+    }
+}
+
+func amountInStock(productID string) int {
+    if productID == "1" {
+        return 11
+    } else if productID == "2" {
+        return 25
+    } else if productID == "3" {
+        return 4
+    } else if productID == "4" {
+        return 6
+    } else if productID == "5" {
+        return 50
+    } else if productID == "6" {
+        return 2
+    } else if productID == "7" {
+        return 0
+    } else if productID == "8" {
+        return 99
+    } else if productID == "9" {
+        return 1
+    } else {
+        return 0
+    }
+}
